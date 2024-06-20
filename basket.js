85ab1d96b9c16b56d673438907a057b4b85cfbf5
Arinzexxx <arinzeinnocent718@gmail.com>
2024-06-20 21:47:56 +0100

    Create basket.js baller

---
+++ basket.js
@@ -0,0 +1,13 @@
+Git functionalities---------====
+
+To upload brand new folders to Git website
+
+ step 1--Create the work folder you want to upload.
+
+ step 2--if text then => {Text (name of folder)}  note it can be any thing like Html and others.
+
+ step 3-- git add . {make sure to add the full stop after the text with one space.}
+
+ step 4-- git commit -m "{add folder discription}"
+
+ step 5-- git push {this is the finall stage}
